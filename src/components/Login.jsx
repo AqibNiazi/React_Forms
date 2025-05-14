1cbbbf60fed8758e76da46d64d07ba819884e648
AqibNiazi <aqibjaved5201@gmail.com>
2025-05-14 15:57:52 +0500

    Reuseable Input component added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,76 +1,74 @@
 import React, { useState } from "react";
+import Input from "./Input";
 export default function Login() {
   const [enteredValue, setEnteredValue] = useState({
     email: "",
     password: "",
   });
 
-const [didEdit, setDidEdit] = useState({
-  email: false,
-  password: false,
-});
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
 
-const emailIsInvalid = didEdit.email && !enteredValue.email.includes("@");
+  const emailIsInvalid = didEdit.email && !enteredValue.email.includes("@");
+  const passwordIsInvalid =
+    didEdit.password && enteredValue.password.trim().length < 6;
 
-const handleInputBlur = (identifier) => {
-  setDidEdit((prevValue) => ({
-    ...prevValue,
-    [identifier]: true,
-  }));
-};
+  const handleInputBlur = (identifier) => {
+    setDidEdit((prevValue) => ({
+      ...prevValue,
+      [identifier]: true,
+    }));
+  };
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  console.log("user email: ", enteredValue.email);
-  console.log("user password: ", enteredValue.password);
-};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("user email: ", enteredValue.email);
+    console.log("user password: ", enteredValue.password);
+  };
 
-const handleChange = (identifier, value) => {
-  setEnteredValue((prevValue) => ({
-    ...prevValue,
-    [identifier]: value,
-  }));
-  setDidEdit((prevValue) => ({
-    ...prevValue,
-    [identifier]: false,
-  }));
-};
-return (
-  <form onSubmit={handleSubmit}>
-    <h2>Login</h2>
+  const handleChange = (identifier, value) => {
+    setEnteredValue((prevValue) => ({
+      ...prevValue,
+      [identifier]: value,
+    }));
+    setDidEdit((prevValue) => ({
+      ...prevValue,
+      [identifier]: false,
+    }));
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
 
-    <div className="control-row">
-      <div className="control no-margin">
-        <label htmlFor="email">Email</label>
-        <input
+      <div className="control-row">
+        <Input
+          label="Email"
           id="email"
           type="email"
           name="email"
           onBlur={() => handleInputBlur("email")}
           value={enteredValue.email}
           onChange={(event) => handleChange("email", event.target.value)}
+          error={emailIsInvalid && "Please enter a valid Email."}
         />
-        <div className="control-error">
-          {emailIsInvalid && <p>Please enter a valid Email.</p>}
-        </div>
-      </div>
-
-      <div className="control no-margin">
-        <label htmlFor="password">Password</label>
-        <input
+        <Input
+          label="Password"
           id="password"
           type="password"
           name="password"
           value={enteredValue.password}
+          onBlur={() => handleInputBlur("password")}
           onChange={(event) => handleChange("password", event.target.value)}
+          error={passwordIsInvalid && "Password must be at least 6 characters."}
         />
       </div>
-    </div>
 
-    <p className="form-actions">
-      <button className="button button-flat">Reset</button>
-      <button className="button">Login</button>
-    </p>
-  </form>
-);
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
 }
